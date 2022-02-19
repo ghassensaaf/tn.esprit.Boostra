140e71598d1c6d717293d12ec58f4f6dbe7bee8b
Ghassen Saaf <saafghassen@gmail.com>
2022-02-19 19:10:41 +0100

    Update BoostraApplication.java

--- src/main/java/tn/esprit/boostra/BoostraApplication.java
+++ src/main/java/tn/esprit/boostra/BoostraApplication.java
@@ -8,6 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableScheduling
+
 public class BoostraApplication {
 
 	public static void main(String[] args) {
