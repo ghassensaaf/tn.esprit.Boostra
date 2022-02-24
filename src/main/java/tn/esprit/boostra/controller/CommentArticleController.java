473990145c7fbf22ab24b6aac726fdbc95bf73f2
ahmed fakhfakh <48211707+fakhfakhahmed@users.noreply.github.com>
2022-02-24 09:57:37 +0100

    Update CommentArticleController.java
    ikhaw

--- src/main/java/tn/esprit/boostra/controller/CommentArticleController.java
+++ src/main/java/tn/esprit/boostra/controller/CommentArticleController.java
@@ -1,7 +1,13 @@
 package tn.esprit.boostra.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +29,30 @@ public class CommentArticleController {
 	public Comment AddArticleComment(@RequestBody Comment comment,@RequestParam("ArticleId") Long ArticleId)
 	{
 		return Cs.ajouterComment(comment,ArticleId);
+	}
+	
+	@PutMapping("/ArticleComment/Update")
+	public Comment UpadateArticleComment(@RequestParam("CommentId") Long CommentId,@RequestBody Comment comment,@RequestParam("ArticleId") Long ArticleId)
+	{
+		Article article=As.GetArticle(ArticleId);
+		comment.setArticle(article);
+		comment.setId(CommentId);
+		return Cs.UpadateComment(comment);
+	}
+	@DeleteMapping("/ArticleComment/delete/{CommentId}")
+	public void DeleteArticleComment(@PathVariable("CommentId") Long CommentId)
+	{
+		Comment comment=Cs.GetComment(CommentId);
+		Cs.DeleteComment(comment);
+	}
+	@GetMapping("/ArticleComment/getAllByArticle/{ArticleId}")
+	public List<Comment> getAll(@PathVariable("ArticleId") Long ArticleId){
+		return Cs.GetAllComment(ArticleId);
+	}
+	
+	@GetMapping("/ArticleComment/GetComment/{CommentId}")
+	public Comment GetArticleComment(@PathVariable("CommentId") Long CommentId){
+		return  Cs.GetComment(CommentId);
 	}
 	
 }
